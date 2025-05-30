87f02e7bcbdc7223e1e1af958c3bb4e70b8c94ac
Dimitar Malinov <malinovd02@gmail.com>
2025-05-30 11:20:27 +0300

    temp cjeck

--- src/api/generate/route.ts
+++ src/api/generate/route.ts
@@ -1,5 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server'
 
+// @ts-nocheck
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 export async function POST(request: NextRequest) {
   try {
     const config = await request.json()
